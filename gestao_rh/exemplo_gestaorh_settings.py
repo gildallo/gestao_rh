606e766dd592b37eb4c843740a3387e304ed047f
Gilcimar Dalló <gildallo@MacBook-Air-de-Gilcimar.local>
2020-06-07 09:32:46 -0300

    Separando valores do arquivo settings

---
+++ gestao_rh/exemplo_gestaorh_settings.py
@@ -0,0 +1,13 @@
+SECRET_KEY = 'fdflksdhfsdbfsdfgbksdf'
+DEBUG = False
+ALLOWED_HOSTS = ['127.0.0.1']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fdfdfd',
+        'USER': 'fdfdfd',
+        'PASSWORD': 'fdfdfd',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
