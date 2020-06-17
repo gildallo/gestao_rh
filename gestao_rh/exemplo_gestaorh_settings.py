d875c332a32e802dcafa176591804b7cef555c1e
Gilcimar Dalló <gilcimar@localhost.localdomain>
2020-06-17 13:30:40 -0300

    Ajuste para multiplos bancos de dados

--- gestao_rh/exemplo_gestaorh_settings.py
+++ gestao_rh/exemplo_gestaorh_settings.py
@@ -1,3 +1,8 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 SECRET_KEY = 'fdflksdhfsdbfsdfgbksdf'
 DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1']
@@ -9,6 +14,10 @@ DATABASES = {
         'PASSWORD': 'fdfdfd',
         'HOST': '127.0.0.1',
         'PORT': '5432',
+    },
+    'antigo': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'dbantigo.sqlite3'),
     }
 }
 #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
